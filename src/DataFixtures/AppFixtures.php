0f19add66a6f96af6037eab5c69e98b93c7b7e0c
AimFried <julien40.der@gmail.com>
2022-01-19 18:21:13 +0100

    Implémentation données Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\LOGEMENT;
+use App\Entity\LOCATAIRE;
 
 class AppFixtures extends Fixture
 {
@@ -23,13 +25,32 @@ class AppFixtures extends Fixture
         //Création des logements
         for($i = 0; $i < $NbrLogements; $i ++)
         {
+            $logement = new Logement();
+            $logement->setNom("Appartement n°",$i);
+            $logement->setAdresse($faker->address);
+            $logement->setVille($faker->region);
+            $logement->setCodePostal($faker->randomNumber(5, true));
+            $logement->setPersMax($faker->numberBetween($min=4, $max=8));
+            $logement->setDescription($faker->realText(200,2));
+            $logement->setEtat($faker->boolean);
 
+            //Enregistrement de l'entité générée
+            $manager->persist($logement);
         }
 
         //Création des locataires
         for($i = 0; $i < $NbrLocataires; $i ++)
         {
-            
+            $locataire = new Locataire();
+            $locataire->setNom($faker->lastName);
+            $locataire->setPrenom($faker->firstName);
+            $locataire->setEmail($faker->freeEmail);
+            $locataire->setTelephone($faker->serviceNumber);
+            $locataire->setAdresse($faker->address);
+            $locataire->setCodePostal($faker->randomNumber(5, true));            
+
+            //Enregistrement de l'entité générée
+            $manager->persist($locataire);
         }
 
         //Création des réservations
