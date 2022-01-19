a7038fb7ed2ba9d99354dfee8c5f6fb60b8948fc
AimFried <julien40.der@gmail.com>
2022-01-19 20:15:40 +0100

    Injection d'indépendance

--- src/Controller/GestionLocationLogementController.php
+++ src/Controller/GestionLocationLogementController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\LOGEMENT;
+use App\Entity\LOCATAIRE;
+use App\Entity\RESERVATION;
+use App\Repository\LOGEMENTRepository;
+use App\Repository\LOCATAIRERepository;
+use App\Repository\RESERVATIONRepository;
+
 
 class GestionLocationLogementController extends AbstractController
 {
     #[Route('/', name: 'authentification')]
     public function authentification(): Response
     {
-        return $this->render('gestion_location_logement/authentification.html.twig', [
-            'controller_name' => 'GestionLocationLogementController',
+        return $this->render('gestion_location_logement/authentification.html.twig',[
+            
         ]);
     }
 
@@ -33,19 +40,21 @@ class GestionLocationLogementController extends AbstractController
     }
 
     #[Route('/logement', name: 'liste_logements')]
-    public function listeLogement(): Response
+    public function listeLogement(LOGEMENTRepository $logementRepository): Response
     {
-        return $this->render('gestion_location_logement/listeLogements.html.twig', [
-            'controller_name' => 'GestionLocationLogementController',
-        ]);
+        $logements = $logementRepository->findAll();
+
+        return $this->render('gestion_location_logement/listeLogements.html.twig', 
+        ['logements' => $logements,]);
     }
 
     #[Route('/locataire', name: 'liste_locataires')]
-    public function listeLocataire(): Response
+    public function listeLocataire(LOCATAIRERepository $locataireRepository): Response
     {
-        return $this->render('gestion_location_logement/listeLocataires.html.twig', [
-            'controller_name' => 'GestionLocationLogementController',
-        ]);
+        $locataires = $locataireRepository->findAll();
+
+        return $this->render('gestion_location_logement/listeLocataires.html.twig', 
+        ['locataires' => $locataires,]);
     }
 
     #[Route('/historique', name: 'historique')]
